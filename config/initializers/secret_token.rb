8eec513f28808da04f7957a8d5c939e625ab6817
Balázs Antal <balazs.antal@pixelephant.hu>
2012-04-17 19:13:38 +0200

    First commit
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelmax::Application.config.secret_token = '37cbfba1feedcf735ad1811dc0a15e87c6078ac1564cf49ed697386eccc8beb2db2197bbf580e18d7a68dd2789f838437c7a9c1b3d106209f97273873690389a'
